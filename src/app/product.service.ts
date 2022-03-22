7b2b8dd3bca11513eb71662854728a0d8b7b94a7
i-am-tvh <100752010+i-am-tvh@users.noreply.github.com>
2022-03-21 18:33:40 -0700

    Album Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import rxjs/add/operator/map;
+import 'rxjs/add/operator/map';
 
 
 @Injectable()
@@ -8,6 +8,7 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
+
   getAlbum(id: number)
   {
     return this._http.get(this._albumUrl).map((response) => response.json());
